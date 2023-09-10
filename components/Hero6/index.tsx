3083e7d7e2b88a2a1d703a2d5219180a02b23543
Bruno Laureano <brunolsantos152@gmail.com>
2023-09-10 01:27:00 -0300

    fix: fixing ui success stories

--- components/Hero6/index.tsx
+++ components/Hero6/index.tsx
@@ -2,20 +2,20 @@ const Hero6 = () => {
   return (
     <>
       <section
-        id="home"
-        className="relative z-10 overflow-hidden border-b-[1px] border-[#D4D4D4] bg-white px-[20px] pb-[65px] pt-[42px] text-[#000000] lg:pb-[129px] lg:pt-[84px]"
+        id="cta"
+        className="relative z-10 overflow-hidden border-b-[1px] border-[#D4D4D4] bg-white px-[60px] pb-[65px] pt-[42px] text-[#000000] lg:pb-[129px] lg:pt-[84px]"
       >
-        <div className="mx-auto xl:w-[1118px]">
-          <div className="flex justify-center text-center text-[32px] font-bold lg:text-[48px] lg:!leading-[77px] xl:text-[64px]">
+        <div className="mx-auto">
+          <div className="flex justify-center text-center  text-[32px] font-bold -tracking-[2%] lg:text-[48px] lg:!leading-[77px] xl:text-[64px]">
             What are you waiting for?
           </div>
-          <div className="mt-[20px] flex justify-center text-center text-[14px] font-medium text-[#959595] lg:mt-[40px] lg:text-[19px] lg:!leading-[44px] xl:text-[28px]">
+          <div className="mx-auto mt-[20px] flex justify-center text-center text-[14px] font-medium -tracking-[2%] text-[#959595] lg:mt-[40px] lg:text-[19px] lg:!leading-[34px] xl:max-w-[1074px] xl:text-[28px]">
             If you are an individual or a team who have the expertise of data
             and Web3 infrastructure, we invite you to apply to become our
             official OpenMesh Expert
           </div>
-          <div className="mt-[52px] md:mt-[104px] md:flex">
-            <div>
+          <div className="mx-auto mt-[52px] max-w-[1200px] bg-yellow md:mt-[104px] md:flex">
+            <div className="flex-1 md:mr-[70px]  lg:mr-[60px]">
               <img
                 src={`${
                   process.env.NEXT_PUBLIC_ENVIRONMENT === 'PROD'
@@ -23,14 +23,14 @@ const Hero6 = () => {
                     : ''
                 }/images/hero3/man.svg`}
                 alt="image"
-                className={`w-[400px] justify-center md:mr-[70px] md:w-[320px] lg:mr-[60px] lg:w-[470px] xl:w-[529px]`}
+                className={`w-full`}
               />
               <div className="mx-auto mt-[16px] flex w-[240px] justify-center text-[14px] font-bold lg:mt-[33px] lg:w-fit lg:text-[19px] lg:!leading-[150%] xl:text-[28px]">
                 Individual Expert
               </div>
             </div>
 
-            <div>
+            <div className="flex-1">
               <img
                 src={`${
                   process.env.NEXT_PUBLIC_ENVIRONMENT === 'PROD'
@@ -38,7 +38,7 @@ const Hero6 = () => {
                     : ''
                 }/images/hero6/people.png`}
                 alt="image"
-                className={`mt-[30px] w-[400px] justify-center md:mt-0 md:w-[320px] lg:w-[470px] xl:w-[529px]`}
+                className={`mt-[30px] w-full  md:mt-0`}
               />
               <div className="mx-auto mt-[16px] flex w-[240px] justify-center text-[14px] font-bold lg:mt-[33px] lg:w-fit lg:text-[19px] lg:!leading-[150%] xl:text-[28px]">
                 Blockchain Consulting Team
