83ca3189f7283663f3738bd5c0b1c1296466bb15
Bruno Laureano <brunolsantos152@gmail.com>
2023-09-14 20:31:32 -0300

    fix: fixing header

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>L3A Ecosystem</title>
+      <title>Openmesh Expert</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Openmesh Expert" />
       <link rel="icon" href="/images/openmesh-logo.png" />
