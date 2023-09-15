6c9b8bb22d53d112ca2e7803923aaac4e666881e
Bruno Laureano <brunolsantos152@gmail.com>
2023-09-15 00:04:02 -0300

    feat: new card

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
           <div className="mt-[32px] flex justify-center text-center lg:mt-[62px]">
             <a
               href={`https://openmesh-expert-community.vercel.app/register`}
-              className="flex cursor-pointer items-center rounded-[5px] border border-[#0354EC] bg-transparent  py-[9px] px-[18px] text-[14px] font-bold !leading-[19px] text-[#0354EC] hover:bg-[#0354EC] hover:text-[#fff] 2xl:py-[11.5px] 2xl:px-[24px] 2xl:text-[16px]"
+              className="flex cursor-pointer items-center rounded-[5px] bg-[#0354EC] py-[9px] px-[18px] text-[14px] font-bold !leading-[19px] text-[#fff] hover:border hover:border-[#0354EC] hover:bg-[#fff] hover:text-[#0354EC] 2xl:py-[11.5px] 2xl:px-[24px] 2xl:text-[16px]"
             >
               Become an expert
             </a>
