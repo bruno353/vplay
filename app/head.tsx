d72490bd52cd9bf9ffc3e322b32adf846c17c852
Bruno Laureano <brunolsantos152@gmail.com>
2023-09-15 10:13:39 -0300

    fix: fixing the about link

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Openmesh Expert</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Openmesh Expert" />
-      <link rel="icon" href="/openmesh-logo.png" />
+      <link rel="icon" href="/openmesh-logo.ico" />
     </>
   )
 }
