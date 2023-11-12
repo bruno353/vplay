67826a11f0df382fc38c5ca8eb1848662cae3d84
Bruno Laureano <brunolsantos152@gmail.com>
2023-11-12 02:42:27 -0300

    feat: just a test

--- components/Hero7/index.tsx
+++ components/Hero7/index.tsx
@@ -36,7 +36,7 @@ const Hero7 = () => {
         id="faqs"
         className="relative z-10 border-b-[1px] border-[#D4D4D4] bg-white px-[20px] pb-[60px] pt-[50px] text-[#000000] lg:pb-[129px] lg:pt-[98px]"
       >
-        <div className="mx-auto xl:w-[1359px]">
+        <div className="mx-auto 2xl:w-[1359px]">
           <div className="text-[16px] font-bold lg:text-[22px] xl:text-[32px]">
             FAQs.{''}{' '}
             <span className="font-semibold text-[#959595]">
