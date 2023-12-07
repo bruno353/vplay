f4e4c4e276de7e334d10c0b834e5df34353d9b60
Bruno Laureano <brunolsantos152@gmail.com>
2023-12-07 09:58:00 -0300

    fix: embedding

---
+++ app/vplay/page.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/no-unknown-property */
+'use case'
+const SignupPage = () => {
+  return (
+    <>
+      <section className="relative z-10 overflow-hidden pt-36 pb-16 md:pb-20 lg:pt-[10px] lg:pb-28">
+        <div className="mx-auto w-fit">
+          <iframe
+            //   style="border: 1px solid rgba(0, 0, 0, 0.1);"
+            width="1600"
+            height="1250"
+            src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Fv4qnRNFwAM95iw3JO2zn8o%2FVP%3Fnode-id%3D35-98%26source%3Demail_invite%26starting-point-node-id%3D1%253A7%26mode%3Ddesign%26t%3DCYZUjrmbnlS77DLb-1"
+            //   allowfullscreen
+          ></iframe>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default SignupPage
